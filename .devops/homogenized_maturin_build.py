0bc9f62a95d3fc08118e6410f5b524048781842c
Dwayne Pryce <dwpryce@microsoft.com>
2020-07-30 14:08:49 -0700

    This shouldn't work

--- .devops/homogenized_maturin_build.py
+++ .devops/homogenized_maturin_build.py
@@ -5,7 +5,7 @@ import subprocess
 def execute() -> str:
     maturin_list_interpreters = ["maturin", "list-python"]
 
-    results = subprocess.run(maturin_list_interpreters, capture_output=True, encoding="utf8", errors="replace")
+    results = subprocess.run(maturin_list_interpreters, capture_output=True, encoding="utf16", errors="replace")
 
     if results.returncode != 0:
         print(
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     interpreter = detect(sys.argv[1], execute())
     results = subprocess.run(
         ["maturin", "build", "--release", "-i", interpreter],
-        encoding="utf8",
+        encoding="utf16",
         capture_output=True
     )
     print(results.stdout)
