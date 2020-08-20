2de94e4a37a8626cc7524935fddfd3743c36a174
Dwayne Pryce <dwpryce@microsoft.com>
2020-08-20 09:46:05 -0700

    WIP: Switching to desktop

--- packages/network_partitions/src/leiden/hierarchical.rs
+++ packages/network_partitions/src/leiden/hierarchical.rs
@@ -2,10 +2,16 @@ use rand::Rng;
 
 use crate::clustering::Clustering;
 use crate::errors::CoreError;
-use crate::network::Network;
+use crate::network::prelude::*;
 use super::leiden::leiden;
+use std::collections::VecDeque;
 
 pub const MAX_CLUSTER_SIZE: u32 = 1000;
+
+pub struct HierarchicalWork {
+    subnetwork: CompactSubnetwork,
+    level: u32,
+}
 
 pub struct HierarchicalCluster {
     node: usize,
@@ -34,23 +40,53 @@ impl HierarchicalCluster {
 }
 
 pub fn hierarchical_leiden<T>(
-    network: &Network,
+    network: &CompactNetwork,
     clustering: Option<Clustering>,
     iterations: Option<usize>,
     resolution: Option<f64>,
     randomness: Option<f64>,
     rng: &mut T,
     use_modularity: bool,
-    max_cluster_size: Option<u32>,
+    max_cluster_size: u32,
 ) -> Result<(bool, Vec<HierarchicalCluster>), CoreError>
     where
         T: Rng + Clone + Send,
 {
-    let max_cluster_size: u32 = max_cluster_size.unwrap_or(MAX_CLUSTER_SIZE);
     let (_, top_level_clustering) = leiden(network, clustering, iterations, resolution, randomness, rng, use_modularity)?;
-    let nodes_by_cluster: Vec<Vec<usize>> = top_level_clustering.nodes_per_cluster();
-    for nodes_within_cluster in nodes_by_cluster {
-
+    // TODO: create top level clustering entries for hierarchical output
+    let mut work_queue: VecDeque<HierarchicalWork> = VecDeque::new();
+    for subnetwork in network.subnetworks_iter(
+        top_level_clustering,
+        Some(max_cluster_size)
+    ) {
+        work_queue.push_back(HierarchicalWork {
+            subnetwork: subnetwork.subnetwork,
+            level: 1
+        });
+    }
+    while !work_queue.is_empty() {
+        let work_item: HierarchicalWork = work_queue.pop_front().unwrap();
+        let (_, subnetwork_clustering) = leiden(
+            &work_item.network,
+            None,
+            iterations,
+            resolution,
+            randomness,
+            rng,
+            use_modularity,
+        )?;
+        // TODO: create hierarchical clustering entry and add to output array
+        for subnetwork in work_item
+            .network
+            .subnetworks_iter(subnetwork_clustering, Some(max_cluster_size)) {
+            // create work item entry
+            work_queue.push_back(
+                HierarchicalWork {
+                    subnetwork: subnetwork.subnetwork,
+                    level: work_item.level + 1,
+                }
+            );
+        }
     }
     return Err(CoreError::ClusterIndexingError);
 }
--- packages/network_partitions/src/leiden/mod.rs
+++ packages/network_partitions/src/leiden/mod.rs
@@ -5,7 +5,7 @@ pub use self::leiden::leiden;
 
 mod full_network_clustering;
 mod full_network_work_queue;
-//mod hierarchical;
+mod hierarchical;
 mod leiden;
 mod neighboring_clusters;
 mod quality_value_increment;
